1eebf24aa6277cf38c6959c2a211834d90fbe272
shepelkate <shepel.catherine@mail.ru>
2023-10-22 14:48:49 +0300

    burger wip

--- assets/scripts/hamburger_header.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".menu");
-
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    hamburger.classList.toggle("hidden");
-    menu.classList.toggle("hidden");
-}
---
+++ assets/scripts/header_hamburger.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector("header .test");
+console.log(hamburger);
+// const menu = document.querySelector(".menu");
+
+// hamburger.addEventListener("click", mobileMenu);
+
+// function mobileMenu() {
+//     hamburger.classList.toggle("active");
+//     // menu.classList.toggle("hidden");
+// }
