4eed9aa2428da145f7c7757b1aa10cfb69ac1036
shepelkate <shepel.catherine@mail.ru>
2023-10-22 16:11:42 +0300

    burger done

--- assets/scripts/header_hamburger.js
+++ assets/scripts/header_hamburger.js
@@ -1,6 +1,5 @@
 const header_hamburger = document.querySelector(".header_hamburger");
 const header_menu = document.querySelector(".header_hamburger-container .header_menu");
-console.log(menu);
 
 header_hamburger.addEventListener("click", mobileMenu);
 
