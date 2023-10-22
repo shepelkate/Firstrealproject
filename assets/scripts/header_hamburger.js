fb096654ea3f8f485517af7a122e24b2d9c48538
shepelkate <shepel.catherine@mail.ru>
2023-10-22 21:35:33 +0300

    Stash

--- assets/scripts/header_hamburger.js
+++ assets/scripts/header_hamburger.js
@@ -1,9 +1,15 @@
 const header_hamburger = document.querySelector(".header__hamburger");
-const header_menu = document.querySelector(".header__mobile-menu");
+const header_mobile_menu = document.querySelector(".header__mobile-menu");
+const header_mobile_menu_link = document.querySelectorAll(".header__mobile-menu a");
 
 header_hamburger.addEventListener("click", mobileMenu);
 
+header_mobile_menu_link.forEach((element) => { 
+    element.addEventListener("click", mobileMenu);
+});
+
+
 function mobileMenu() {
     header_hamburger.classList.toggle("active");
-    header_menu.classList.toggle("active");
+    header_mobile_menu.classList.toggle("active");
 }
